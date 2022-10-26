fdf6415adc18a947a14b541e12fe63806f0c69ab
Mihaela Diaconu <diaconumihaela748@yahoo.com>
2022-10-26 19:35:51 +0100

    finished answer checking and timer

--- Code/SmileGame/Startup.Designer.cs
+++ Code/SmileGame/Startup.Designer.cs
@@ -29,8 +29,13 @@ namespace SmileGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.imgResult = new System.Windows.Forms.PictureBox();
             this.btnNewGame = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.answerTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.imgResult)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,17 +62,52 @@ namespace SmileGame
             this.btnNewGame.UseVisualStyleBackColor = true;
             this.btnNewGame.Click += new System.EventHandler(this.NewGame_Click);
             // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(75, 14);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(125, 27);
+            this.txtAnswer.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 17);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(57, 20);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Answer";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(206, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(94, 29);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.CheckAnswer_Click);
+            // 
+            // answerTimer
+            // 
+            this.answerTimer.Interval = 1000;
+            this.answerTimer.Tick += new System.EventHandler(this.AnswerTimer_Tick);
+            // 
             // Startup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtAnswer);
             this.Controls.Add(this.btnNewGame);
             this.Controls.Add(this.imgResult);
             this.Name = "Startup";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imgResult)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +115,10 @@ namespace SmileGame
 
         private System.Windows.Forms.PictureBox imgResult;
         private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Timer answerTimer;
     }
 }
 
--- Code/SmileGame/Startup.cs
+++ Code/SmileGame/Startup.cs
@@ -13,6 +13,9 @@ namespace SmileGame
 {
     public partial class Startup : Form
     {
+        int answer = 0;
+        int remainingSeconds = 10;
+
         public Startup()
         {
             InitializeComponent();
@@ -22,6 +25,40 @@ namespace SmileGame
         {
             GameApiEntity game = await SmileGameApi.GetDataAsync();
             imgResult.Image = Image.FromFile(game.Question.Substring(game.Question.LastIndexOf("/") + 1));
+            answer = game.Solution;
+            answerTimer.Start();
+        }
+
+        private void CheckAnswer_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAnswer.Text) == false)
+            {
+                if (txtAnswer.Text == answer.ToString())
+                {
+                    MessageBox.Show("Correct!");
+                    answerTimer.Stop();
+                    remainingSeconds = 10;
+                }
+                else
+                    MessageBox.Show("Inccorect!");
+            }
+            else
+                MessageBox.Show("Answer cannot be empty!");
+        }
+       
+        private void AnswerTimer_Tick(object sender, EventArgs e)
+        {
+            if (remainingSeconds > 1)
+            {
+                remainingSeconds = remainingSeconds - 1;
+                Text = remainingSeconds.ToString();
+            }
+            else
+            {
+                answerTimer.Stop();
+                remainingSeconds = 10;
+                MessageBox.Show("Time's up! Correct answer was " + answer);
+            }
         }
     }
 }
